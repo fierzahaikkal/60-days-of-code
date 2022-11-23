94cd860eb6dbca38dd5f80fac5e5d59fc356f414
fierzahaikkal <ojogames.id@gmail.com>
2022-11-23 23:25:25 +0700

    final day1-day8

--- Day1/index.js
+++ Day1/index.js
@@ -3,9 +3,9 @@ const nav = document.querySelector("nav");
 const mobile_btn_exit = document.getElementById("exit-cta");
 
 mobile_btn.addEventListener("click", () => {
-  nav.classList.add("mobile-btn");
+  nav.classList.add("mobile-menu1");
 });
 
 mobile_btn_exit.addEventListener("click", () => {
-  nav.classList.remove("mobile-btn");
+  nav.classList.remove("mobile-menu1");
 });
