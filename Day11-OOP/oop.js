c0b2c020084ffef99696c767c4c566eda6448a55
fierzahaikkal <ojogames.id@gmail.com>
2022-12-01 22:55:34 +0700

    DOM commit

--- Day11-OOP/oop.js
+++ Day11-OOP/oop.js
@@ -13,17 +13,21 @@
 // person1.sendMessage("Selamat malam", "arga");
 
 function SignUp(uname, passw, mail) {
-  // const symbol = /^[A-Za-z]\w{7,14}$/;
+  var regex = /^[A-Za-z]\w{7,14}$/;
   this.username = uname;
   this.password = passw;
   this.mail = mail;
 
-  if (this.password < 8) {
+  if (this.password.length < 8) {
     alert("Password must have 8 character");
+    this.password = "";
+    return false;
+  } else if (!regex.test(this.password)) {
+    alert(`Password must contain ${regex}`);
     this.password = "";
     return false;
   }
 }
 
-const user1 = new SignUp("Rangga", "ras2", "xyz@gmail.com");
+const user1 = new SignUp("Rangga", "rasaAasss212aa", "xyz@gmail.com");
 console.log(user1);
