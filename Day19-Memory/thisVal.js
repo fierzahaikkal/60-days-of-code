827a3dd2f27d32a7710732c45922e53c4b86c5d3
fierzahaikkal <ojogames.id@gmail.com>
2022-12-18 18:13:36 +0700

    adding newline commit

--- Day19-Memory/thisVal.js
+++ Day19-Memory/thisVal.js
@@ -16,6 +16,7 @@ let john = {
     console.log(
       this.firstName + " " + this.lastName + " is time travel to " + year
     );
+    console.log(this);
   },
 };
 john.timeTravel(1998);
@@ -46,3 +47,37 @@ getBackInTime(1774);
 why undefined? because there is no object for getBackInTime, it makes
 this context refers as window object (global)
 */
+
+//Value of this in method invoked asynchronously
+let flux = document.getElementById("flux-capacitor");
+flux.addEventListener("click", john.timeTravel);
+//Output: undefined undefined is time travelling to [object MouseEvent]
+/*
+permasalahan:
+1. Tidak mendefinisikan argumen tahun pada .timeTravel()
+2. Object yang dituju bukan john tetapi flux-capacitor
+karena method yang kita pakai berperan sebagai event handler, sehingga
+argumen pada event click akan diteruskan sebagai argumen pertama di event handler
+3. Undefined Undefined karena object yang dituju adalah flux-capacitor
+sehingga tidak ada property firstName dan lastName didalamnya
+*/
+
+// Fourth: Value of this Inside Constructor Functions
+
+//when we create new object instance using Constructor,
+//this value inside the function refers to its object
+
+const TimeTraveler = (fname, lname, year) => {
+  this.firstName = fname;
+  this.lastName = lname;
+  this.year = year;
+
+  //Constructor function return
+  //newly created object for us
+  // unless we specifically return something else
+};
+
+let person = new TimeTraveler("John", "Doe", 1995);
+console.log(
+  person.firstName + " " + person.lastName + " is time travel to " + person.year
+);
